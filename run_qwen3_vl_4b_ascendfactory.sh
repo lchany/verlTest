b0b5faafcf1ea0a2cf1e68f8c496273168b0ed63
lchany <lchany@github.com>
2026-04-17 17:24:22 +0800

    feat: add ascendfactory-cli training script for Qwen3-VL-4B

---
+++ run_qwen3_vl_4b_ascendfactory.sh
@@ -0,0 +1,18 @@
+set -x
+
+source /usr/local/Ascend/ascend-toolkit/set_env.sh
+source /usr/local/Ascend/nnal/atb/set_env.sh
+
+export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+
+ascendfactory-cli train /home/l30002999/yaml/verl_qwen3_vl-4b_ppo_fsdp.yaml \
+    --backend_config.actor_rollout_ref.model.path=/data/nfs/model/Qwen3-VL-4B-Instruct \
+    --backend_config.data.train_files=/data/nfs/trainning/datasets/performance/geo3k/train.parquet \
+    --backend_config.data.val_files=/data/nfs/trainning/datasets/performance/geo3k/test.parquet \
+    --backend_config.trainer.save_freq=-1 \
+    --backend_config.trainer.total_training_steps=5 \
+    --af_output_dir=/data/nfs/l30002999/checkpoints/output/qwen3vl_4b_grpo_2npu \
+    --env.MASTER_ADDR=localhost \
+    --env.NNODES=1 \
+    --env.NODE_RANK=0 \
+    | tee /home/l30002999/qwen3_vl-4b_grpo_fsdp_A2_start.log
