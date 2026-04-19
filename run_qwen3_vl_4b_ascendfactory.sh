51b614427049ac02e139afa61a1918de94ae1c0a
lchany <lchany@github.com>
2026-04-19 15:41:43 +0800

    fix: correct yaml file path

--- run_qwen3_vl_4b_ascendfactory.sh
+++ run_qwen3_vl_4b_ascendfactory.sh
@@ -5,7 +5,7 @@ source /usr/local/Ascend/nnal/atb/set_env.sh
 
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
-ascendfactory-cli train /home/l30002999/yaml/verl_qwen3_vl-4b_ppo_fsdp.yaml \
+ascendfactory-cli train /home/l30002999/verl_qwen3_vl-4b_ppo_fsdp.yaml \
     --backend_config.actor_rollout_ref.model.path=/data/nfs/model/Qwen3-VL-4B-Instruct \
     --backend_config.data.train_files=/data/nfs/trainning/datasets/performance/geo3k/train.parquet \
     --backend_config.data.val_files=/data/nfs/trainning/datasets/performance/geo3k/test.parquet \
