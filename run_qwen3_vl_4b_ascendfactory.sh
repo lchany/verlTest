3b31cb4fb1c56ce1c3cfcf1541094b8bc3fe8e4b
lchany <lchany@github.com>
2026-04-20 11:16:43 +0800

    fix: reduce CPU memory to prevent Ray OOM kills
    - Disable RAY memory monitor to prevent SIGTERM (RAY_memory_monitor_refresh_ms=0)
    - Limit Ray object store to 10GB (RAY_object_store_memory=10737418240)
    - Disable ref model param_offload to reduce CPU memory
    - Reduce ref log_prob_micro_batch_size_per_gpu from 128 to 16

--- run_qwen3_vl_4b_ascendfactory.sh
+++ run_qwen3_vl_4b_ascendfactory.sh
@@ -4,6 +4,8 @@ source /usr/local/Ascend/ascend-toolkit/set_env.sh
 source /usr/local/Ascend/nnal/atb/set_env.sh
 
 export ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export RAY_memory_monitor_refresh_ms=0
+export RAY_object_store_memory=10737418240
 
 ascendfactory-cli train /home/l30002999/verl_qwen3_vl-4b_ppo_fsdp.yaml \
     --backend_config.actor_rollout_ref.model.path=/data/nfs/model/Qwen3-VL-4B-Instruct \
@@ -11,6 +13,8 @@ ascendfactory-cli train /home/l30002999/verl_qwen3_vl-4b_ppo_fsdp.yaml \
     --backend_config.data.val_files=/data/nfs/trainning/datasets/performance/geo3k/test.parquet \
     --backend_config.trainer.save_freq=50 \
     --backend_config.actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    --backend_config.actor_rollout_ref.ref.fsdp_config.param_offload=False \
+    --backend_config.actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     --af_output_dir=/data/nfs/l30002999/checkpoints/output/qwen3vl_4b_grpo_2npu \
     --env.MASTER_ADDR=localhost \
     --env.NNODES=1 \
